8525975c5b51a5f889b23bc12be1ca8157382482
Amos Brocco <amos.brocco@gmail.com>
2021-10-29 09:34:23 +0200

    RefCell implementation

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,66 @@
-#![feature(once_cell)]
-use std::{lazy::SyncLazy, sync::Mutex};
+#![feature(cell_leak)]
+use std::{cell::{RefCell, RefMut}};
 use sha2::{Sha256, Sha512, Digest};
-#[macro_use]
 use lazy_static::lazy_static;
 
-static THEHASHER : SyncLazy<Mutex<Box<dyn Hasher + Send + Sync>>> 
-                    = SyncLazy::new(|| Mutex::new(Box::new(Hasher512::new())));
-
 lazy_static! {
+    static ref THEHASHER : Box<dyn Hasher + Sync + Send> = Box::new(Hasher256::new());
     static ref SHA256ALGO: Box<Sha256> = Box::new(Sha256::new());
     static ref SHA512ALGO: Box<Sha256> = Box::new(Sha256::new());
 }
 
-
-trait Hasher {
-    fn hash_str(&mut self, data : &str) -> String;
+trait Hasher : Sync + Send {
+    fn hash_str(&self, data : &str) -> String;
 }
 
 struct Hasher256 {
-    hasher : crypto::sha2::Sha256
+    hasher : RefCell<crypto::sha2::Sha256>
 }
 
 impl Hasher256 {
     pub fn new() -> Self {
         Self {
-            hasher : crypto::sha2::Sha256::new()
+            hasher : RefCell::new(crypto::sha2::Sha256::new())
         }
     }
 }
 
+unsafe impl Sync for Hasher256 {
+}
+
 impl Hasher for Hasher256 {
-    fn hash_str(&mut self, data : &str) -> String {
-        crypto::digest::Digest::input(&mut self.hasher, data.as_bytes());
-        crypto::digest::Digest::result_str(&mut self.hasher)
+    fn hash_str(&self, data : &str) -> String {
+        let ch = RefMut::leak(self.hasher.borrow_mut());
+        crypto::digest::Digest::reset(ch);
+        crypto::digest::Digest::input(ch, data.as_bytes());
+        crypto::digest::Digest::result_str(ch)
     }
 }
 
 struct Hasher512 {
-    hasher : crypto::sha2::Sha512
+    hasher : RefCell<crypto::sha2::Sha512>
 }
 
 impl Hasher512 {
     pub fn new() -> Self {
         Self {
-            hasher : crypto::sha2::Sha512::new()
+            hasher : RefCell::new(crypto::sha2::Sha512::new())
         }
     }
 }
 
+unsafe impl Sync for Hasher512 {
+}
+
 impl Hasher for Hasher512 {
-    fn hash_str(&mut self, data : &str) -> String {
-        crypto::digest::Digest::input(&mut self.hasher, data.as_bytes());
-        crypto::digest::Digest::result_str(&mut self.hasher)
+    fn hash_str(&self, data : &str) -> String {
+        let ch = RefMut::leak(self.hasher.borrow_mut());
+        crypto::digest::Digest::reset(ch);
+        crypto::digest::Digest::input(ch, data.as_bytes());
+        crypto::digest::Digest::result_str(ch)
     }
 }
 
 fn main() {
-     println!("Hash value {}", THEHASHER.lock().unwrap().hash_str("hello world"));
+    println!("Hash value {}", THEHASHER.hash_str("hello world"));
 }
